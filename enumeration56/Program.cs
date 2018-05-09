4543ffa12ebb2fea00866543a491e817aa92b0b4
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-08 22:50:41 -0700

    Add project files.

---
+++ enumeration56/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumeration56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
